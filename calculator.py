e4c1f986b9dde0e4516ea0513ab16e68281648fc
Tomás Antunes <tomasantunes@gmail.com>
2022-02-19 22:22:20 +0000

    Calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first digit: "))
+op = input("Enter operator: ")
+b = int(input("Enter second digit: "))
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b)
