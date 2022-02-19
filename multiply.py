5fd91fb5c3712458b3b7c8ab63d29810f7975b0d
Tomás Antunes <tomasantunes@gmail.com>
2022-02-19 21:59:22 +0000

    Update multiply

--- multiply.py
+++ multiply.py
@@ -1,7 +1,11 @@
-import math
-
 def multiply(a, b):
     return a * b
 
 def multiplyList(a):
-    return math.prod(a)
+    x = 1
+    for i in a:
+        x *= i
+    return x
+
+print(multiply(2, 3))
+print(multiplyList([1, 2, 3]))
